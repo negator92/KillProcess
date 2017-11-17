8a1211c41d6b2bb2e6978b24a2f5c5b8757fdf24
Vincent Neparidze <nva@akmetron.ru>
2017-11-17 14:59:55 +0300

    First commit

--- KillProcess/Program.cs
+++ KillProcess/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace KillProcess
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            do
+            {
+                Console.WriteLine("Enter name process to kill: ");
+                string processName = Console.ReadLine();
+                if (processName == "")
+                    return;
+                else if(processName.Contains(".exe"))
+                    processName.Replace(".exe", "");
+                try
+                {
+                    foreach(Process p in Process.GetProcessesByName(processName))
+                        p.Kill();
+                    Console.WriteLine($"{processName} killed!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error, {ex.Message}, {ex.StackTrace}");
+                }
+                Console.WriteLine("Enter q to quit or enter to continue:");
+            } while (Console.ReadLine() != "q");
         }
     }
 }
